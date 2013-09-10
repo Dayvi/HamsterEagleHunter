deabf34ef7db2937f6f98750390aa32e9cdc453c
Reid <ragiko@gmail.com>
2013-09-10 02:26:07 -0400

    New 4.2.1 Unity update to the repo

--- HEH/Assets/Scripts/Player/PlayerOne/PlayerOneGUI.cs
+++ HEH/Assets/Scripts/Player/PlayerOne/PlayerOneGUI.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class PlayerOneGUI : MonoBehaviour {
 	
-	public Texture2D heartIndicator;
+	public Texture2D heartIndicatorOne;
+	public Texture2D heartIndicatorTwo;
+	public Texture2D heartIndicatorThree;
 
 	void OnGUI()
 	{
-		GUI.DrawTexture(new Rect(10, 10, 60, 120), heartIndicator, ScaleMode.ScaleToFit, true, 1.0F);
-		GUI.DrawTexture(new Rect(80, 10, 60, 120), heartIndicator, ScaleMode.ScaleToFit, true, 1.0F);
-		GUI.DrawTexture(new Rect(150, 10, 60, 120), heartIndicator, ScaleMode.ScaleToFit, true, 1.0F);
+		GUI.DrawTexture(new Rect(10, 10, 60, 120), heartIndicatorOne, ScaleMode.ScaleToFit, true, 1.0F);
+		GUI.DrawTexture(new Rect(80, 10, 60, 120), heartIndicatorTwo, ScaleMode.ScaleToFit, true, 1.0F);
+		GUI.DrawTexture(new Rect(150, 10, 60, 120), heartIndicatorThree, ScaleMode.ScaleToFit, true, 1.0F);
 	}
 }
